55716d10e5756ef949658f46da360232f668e89c
Sean Noway <sean.m.holian@gmail.com>
2023-04-28 16:09:43 -0400

    added questions into quiestions.js

---
+++ assets/question.js
@@ -0,0 +1,83 @@
+var questions = [{
+    "question": "What is Ash's last name in the Pokemon anime series?",
+    "option1": "Catch'Em",
+    "option2": "Smith",
+    "option3": "Ketchum",
+    "option4": "Ketchup",
+    "asnwer": "3"
+}, {
+    "question": "What was Ash's first Pokemon?",
+    "option1": "Pikachu",
+    "option2": "Caterpie",
+    "option3": "Squirtle",
+    "option4": "Pidgey",
+    "asnwer": "1"
+}, {
+    "question": "What year did the first Pokemon movie come out?",
+    "option1": "1999",
+    "option2": "2000",
+    "option3": "1996",
+    "option4": "1998",
+    "asnwer": "4"
+}, {
+    "question": "What Pokedex number is Pikachu?",
+    "option1": "24",
+    "option2": "25",
+    "option3": "26",
+    "option4": "007",
+    "asnwer": "2"
+}, {
+    "question": "Charizard is what type of Pokemon?",
+    "option1": "Water",
+    "option2": "Char",
+    "option3": "Fire",
+    "option4": "Dragon",
+    "asnwer": "3"
+}, {
+    "question": "How many original Pokemon were there?",
+    "option1": "151",
+    "option2": "150",
+    "option3": "149",
+    "option4": "350",
+    "asnwer": "1"
+}, {
+    "question": "Who is #1 in the Pokedex?",
+    "option1": "Charmander",
+    "option2": "Pikachu",
+    "option3": "Squirtle",
+    "option4": "Bulbasaur",
+    "asnwer": "4"
+}, {
+    "question": "The English motto of Pokemon is?",
+    "option1": "Catch'Em",
+    "option2": "Gotta Catch 'Em All!",
+    "option3": "Do Or Do Not, There Is No Try",
+    "option4": "I Done Caught 'Em",
+    "asnwer": "2"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
