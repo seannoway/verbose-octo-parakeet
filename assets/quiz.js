43a0a554c9800bb201529b049927813dcef9daeb
Sean Noway <sean.m.holian@gmail.com>
2023-04-29 15:50:29 -0400

    finsihed up javascript for loading questions, needs some trouble shooting

--- assets/quiz.js
+++ assets/quiz.js
@@ -2,7 +2,7 @@ var currentQuestion = 0;
 var score = 0;
 var totalQuestions = questions.length;
 
-var container = document.getElementById('quizcontainer');
+var container = document.getElementById('quizContainer');
 var questionEl = document.getElementById('question');
 var opt1 = document.getElementById('opt1');
 var opt2 = document.getElementById('opt2');
@@ -12,7 +12,7 @@ var nextButton = document.getElementById('nextButton');
 var resultCont = document.getElementById('result');
 
 function loadQuestion (questionIndex) {
-    var q = questions(questionIndex);
+    var q = questions[questionIndex];
     questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
     opt1.textContent = q.option1;
     opt2.textContent = q.option2;
@@ -20,3 +20,29 @@ function loadQuestion (questionIndex) {
     opt4.textContent = q.option4;
 };
 
+function loadNextQuestion () {
+    var selectedOption = document.querySelector('input[type=radio]:checked');
+    if(!selectedOption){
+        alert('Please select your answer!');
+        return;
+    }
+    var answer = selectedOption.value;
+    if(questions[currentQuestion].answer == answer){
+        score += 1;
+    }
+    selectedOption.checked = false;
+    // timeLeft -= 5; // Takes 5 seconds off the timer if answer is incorrect
+    currentQuestion++;
+    if(currentQuestion = totalQuestions -1){
+        nextButton.textContent = "Finish"; //Changes next button to "finish" when on the last question
+    }
+    if(currentQuestion == totalQuestions){
+        container.style.display = 'none'; //hides container when questions are done
+        resultCont.style.display = '';
+        resultCont.textContent = 'Your Score: ' + score; //displays score at end of quiz
+        return;
+    }
+    loadQuestion(currentQuestion);
+}
+
+loadQuestion(currentQuestion); //manually loads first question will add start button later
