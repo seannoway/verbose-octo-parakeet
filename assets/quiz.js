aae529fc456f4b68e0b38457fddaca5c14004eac
Sean Noway <sean.m.holian@gmail.com>
2023-04-29 15:11:18 -0400

    added var's for linking quiz.js to index.html

---
+++ assets/quiz.js
@@ -0,0 +1,9 @@
+var currentQuestion = 0;
+var score = 0;
+
+var container = document.getElementById('quizcontainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt1 = document.getElementById('opt2');
+var opt1 = document.getElementById('opt3');
+var opt1 = document.getElementById('opt4');
