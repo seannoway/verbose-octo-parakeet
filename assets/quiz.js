ca85bcbd7e0708b8611f205515df543de89f6b64
Sean Noway <sean.m.holian@gmail.com>
2023-04-29 15:14:45 -0400

    added more variables into quiz.js to link

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,9 +1,14 @@
 var currentQuestion = 0;
 var score = 0;
+var totalQuestions = questions.length;
 
 var container = document.getElementById('quizcontainer');
 var questionEl = document.getElementById('question');
 var opt1 = document.getElementById('opt1');
-var opt1 = document.getElementById('opt2');
-var opt1 = document.getElementById('opt3');
-var opt1 = document.getElementById('opt4');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+
+
