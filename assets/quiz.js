3980678c32744e45cb3679a2b093d8c81428bcf3
Sean Noway <sean.m.holian@gmail.com>
2023-05-01 14:55:26 -0400

    fixed function to render quiz

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,7 +1,66 @@
+var questions = [{
+    "question": "What is Ash's last name in the Pokemon anime series?",
+    "option1": "Catch'Em",
+    "option2": "Smith",
+    "option3": "Ketchum",
+    "option4": "Ketchup",
+    "answer": "3"
+}, {
+    "question": "What was Ash's first Pokemon?",
+    "option1": "Pikachu",
+    "option2": "Caterpie",
+    "option3": "Squirtle",
+    "option4": "Pidgey",
+    "answer": "1"
+}, {
+    "question": "What year did the first Pokemon movie come out?",
+    "option1": "1999",
+    "option2": "2000",
+    "option3": "1996",
+    "option4": "1998",
+    "answer": "4"
+}, {
+    "question": "What Pokedex number is Pikachu?",
+    "option1": "24",
+    "option2": "25",
+    "option3": "26",
+    "option4": "007",
+    "answer": "2"
+}, {
+    "question": "Charizard is what type of Pokemon?",
+    "option1": "Water",
+    "option2": "Char",
+    "option3": "Fire",
+    "option4": "Dragon",
+    "answer": "3"
+}, {
+    "question": "How many original Pokemon were there?",
+    "option1": "151",
+    "option2": "150",
+    "option3": "149",
+    "option4": "350",
+    "answer": "1"
+}, {
+    "question": "Who is #1 in the Pokedex?",
+    "option1": "Charmander",
+    "option2": "Pikachu",
+    "option3": "Squirtle",
+    "option4": "Bulbasaur",
+    "answer": "4"
+}, {
+    "question": "The English motto of Pokemon is?",
+    "option1": "Catch'Em",
+    "option2": "Gotta Catch 'Em All!",
+    "option3": "Do Or Do Not, There Is No Try",
+    "option4": "I Done Caught 'Em",
+    "answer": "2"
+}
+
+]
 var currentQuestion = 0;
 var score = 0;
 var totalQuestions = questions.length;
-
+// var questionIndex=0;
 var container = document.getElementById('quizContainer');
 var questionEl = document.getElementById('question');
 var opt1 = document.getElementById('opt1');
@@ -19,7 +78,7 @@ function loadQuestion (questionIndex) {
     opt3.textContent = q.option3;
     opt4.textContent = q.option4;
 };
-
+// loadQuestion();
 function loadNextQuestion () {
     var selectedOption = document.querySelector('input[type=radio]:checked');
     if(!selectedOption){
@@ -33,7 +92,7 @@ function loadNextQuestion () {
     selectedOption.checked = false;
     // timeLeft -= 5; // Takes 5 seconds off the timer if answer is incorrect
     currentQuestion++;
-    if(currentQuestion = totalQuestions -1){
+    if(currentQuestion == totalQuestions -1){
         nextButton.textContent = "Finish"; //Changes next button to "finish" when on the last question
     }
     if(currentQuestion == totalQuestions){
